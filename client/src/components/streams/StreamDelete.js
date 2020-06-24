65697e3c8b6e52e7c5e1e805cb00c722a7ee9a00
OjukwuEbuka <ebukaojukwu23@gmail.com>
2020-06-24 08:22:43 +0100

    Created a Portal (Modal) and refactored the StreamDelete Component

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import Modal from '../Modal';
+import history from '../../history';
+import { connect } from 'react-redux';
+import {fetchStream} from '../../actions'
 
-const StreamDelete = () => {
-    return <div>StreamDelete</div>
+class StreamDelete extends React.Component {
+
+    componentDidMount(){
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    actions = (
+        <React.Fragment>
+            <button className="ui button negative">Delete</button>
+            <button className="ui button">Cancel</button>
+        </React.Fragment>
+    )
+
+    render(){
+        if(!props.stream) return null;
+        
+        return (
+        <div>StreamDelete
+            <Modal 
+                title="Delete Stream" 
+                content="Are you sure you want to delete this stream?"
+                actions={this.actions}
+                onDismiss={()=>history.push('/')}
+            />
+        </div>
+        )
+    }
 }
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+    return {stream: state.streams.find(stream => stream.id === ownProps.match.params.id)}
+}
+
+export default connect(mapStateToProps, {fetchStream})(StreamDelete);
